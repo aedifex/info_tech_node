857feb5eff27a0cc87b468f1b3f62022ff16011f
aedifex <chris@circleci.com>
2019-11-13 13:59:58 +0000

    Initial commit

---
+++ test/secondary_tests.js
@@ -0,0 +1,59 @@
+
+var request = require('request');
+
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+
+const app = require("../app.js");
+
+const { expect } = chai;
+
+
+chai.use(chaiHttp);
+
+const port = process.env.PORT || 3000;
+const version = process.env.VERSION || "123.123";
+
+chai.use(chaiHttp);
+describe("Server!", () => {
+  it("welcomes user to the api", done => {
+    chai
+      .request(app)
+      .get("/version")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        console.log(res.body.version)
+        expect(res.body.version).to.equals(version);
+        // expect(res.body.message).to.equals("Welcome To Testing API");
+        done();
+      });
+  });
+});
+
+
+/*
+  it("adds 2 numbers", done => {
+    chai
+      .request(app)
+      .post("/add")
+      .send({ num1: 5, num2: 5 })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.status).to.equals("success");
+        expect(res.body.result).to.equals(10);
+        done();
+      });
+  });
+});
+
+
+
+
+it('Main page status', function(done) {
+    request(`http://localhost:${port}/version` , function(error, response, body) {
+    	console.log("Version: " + response.body);
+        expect(response.body.status).to.equal(status);
+        done();
+    });
+});
+*/
