96376b6586ab456eaf43519da41683d7fbac05d8
aedifex <chris@circleci.com>
2019-11-13 17:42:09 +0000

    Initial commit

---
+++ test/primary_tests.js
@@ -0,0 +1,20 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+// We'll run two files, with two tests each
+// primary tests will test the primary endpoints
+// and routes.
+
+const port = process.env.PORT || 3000;
+
+console.log("Greeting from: " + (process.env.CIRCLE_JOB || null) + "running on Node index: " + (process.env.CIRCLE_NODE_INDEX || null))
+
+it('Main page status', function(done) {
+    request(`http://localhost:${port}` , function(error, response, body) {
+    	// console.log(body)
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+console.log('Port:' , port)
