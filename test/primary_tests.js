74057befb5dbda6b41a0ea5cb2f1aaa2a2efd91e
aedifex <chris@circleci.com>
2019-11-15 15:12:38 +0000

    Initial commit

---
+++ test/primary_tests.js
@@ -0,0 +1,40 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+// We'll run two files, with two tests each
+// primary tests will test the primary endpoints
+// and routes.
+
+const port = process.env.PORT || 3000;
+
+console.log("Greeting from: " + (process.env.CIRCLE_JOB || null) + "running on Node index: " + (process.env.CIRCLE_NODE_INDEX || null))
+
+// Attempt at 'sleep'
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+  console.log('Taking a break...');
+  await sleep(60000);
+  console.log('Two seconds later, showing sleep in a loop...');
+
+  // Sleep in loop
+  for (let i = 0; i < 5; i++) {
+    if (i === 3)
+      await sleep(60000);
+    console.log(i);
+  }
+}
+
+demo();
+
+it('Main page status', function(done) {
+    request(`http://localhost:${port}` , function(error, response, body) {
+    	// console.log(body)
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+console.log('Port:' , port)
